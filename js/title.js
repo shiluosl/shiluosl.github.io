c6482a1866330fc7c3ca25b210d9bff6f3a41844
shiluosl <113872027+shiluosl@users.noreply.github.com>
2022-09-19 15:35:08 +0000

    切换页面音乐不暂停 Updated By Github Actions

--- js/title.js
+++ js/title.js
@@ -1,4 +1,4 @@
-//动态标题
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="/assets/js/Meting.min.js"></script>//动态标题
 var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
